203630695263b03537aa87dd6a73701341df1105
Michalkcb <michalkcb@gmail.com>
2024-01-03 18:48:05 +0100

    init ticTacToe

---
+++ wyzwaniePython/ex01/main2.py
@@ -0,0 +1,16 @@
+x = "Splinter"
+
+if x == "Donatello":
+    print ("Nazywasz sie ", x)
+
+elif x == "Raphaello":
+    print ("Nazywasz sie ", x)
+
+elif x == "Michaelangelo":
+    print ("Nazywasz sie ", x)
+
+elif x == "Leonardo":
+    print ("Nazywasz sie ", x)
+
+else:
+    print ("Nie nazywasz się jak jeden z Wojowniczych Żółwi Ninja. Nazywasz się ", x)
