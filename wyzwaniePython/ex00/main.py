fe20e6c72ca5d992144c8cf7a4ca3e88c658de55
Michalkcb <michalkcb@gmail.com>
2024-01-03 16:29:06 +0100

    init python ex00

---
+++ wyzwaniePython/ex00/main.py
@@ -0,0 +1,17 @@
+def mojTekst():
+  print("Czesć, jestem twoim pierwszym programem w Pythonie!")
+  
+def pozdrowienia():
+  print("Pozdrowienia od funkcji")
+  
+def dodawanie():
+    return 2+5
+    
+
+mojTekst()
+pozdrowienia()
+print("wynik mojej funkcji dodawania to:", dodawanie())
+
+x = "Michał"
+y = 1982
+print("Cześć, ", x, " twój rok urodzenia to: ", y)
