fa6ee374c19a8d7cedc8d65f6a3343e3e406cdf9
Michalkcb <michalkcb@gmail.com>
2024-01-03 18:32:22 +0100

    another python lesson

---
+++ wyzwaniePython/ex01/main.py
@@ -0,0 +1,19 @@
+x = "Michał"
+
+if x == "Michał":
+    print ("Nazywasz się", x)
+
+else:
+    print ("Nie nazywasz się", x)
+
+a = "Imietwojegoznajomego"
+b = "Twojeimie"
+
+if a == "Twojeimie":
+    print ("Nazywasz się ", a)
+
+elif b == "Twojeimie":
+    print ("Nazywaszsię ", b)
+
+else:
+    print ("Żadna zmienna nie przechowuje twojego imienia")
