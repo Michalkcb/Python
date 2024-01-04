febbafeea03ac80fb7c7017ddde56e8b58dd4ce5
Michalkcb <michalkcb@gmail.com>
2024-01-04 15:55:40 +0100

    add instruction, init board

---
+++ wyzwaniePython/ticTacToe/tictactoe.py
@@ -0,0 +1,17 @@
+board = ["_","_","_",
+         "_","_","_",
+         "_","_","_",]
+
+def display_board():
+    print("\n")
+    print(board[0] + " | " + board[1] + " | "+ board[2] + " | " + " 1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | "+ board[5] + " | " + " 3 | 4 | 5")
+    print(board[6] + " | " + board[7] + " | "+ board[8] + " | " + " 6 | 7 | 8")
+    print("\n")
+
+display_board()
+
+def handle_turn(player):
+    
+
+
