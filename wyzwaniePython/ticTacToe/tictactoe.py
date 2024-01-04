4ad5b941f760875711e9d9bc198ecf174b7ce722
Michalkcb <michalkcb@gmail.com>
2024-01-04 16:03:56 +0100

    add handle turn

--- wyzwaniePython/ticTacToe/tictactoe.py
+++ wyzwaniePython/ticTacToe/tictactoe.py
@@ -12,6 +12,16 @@ def display_board():
 display_board()
 
 def handle_turn(player):
-    
+    print(payer + "twoja kolej")
+    position = input("wybierz pozycję od 1 do 9: ")
 
+    while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        position = input("Błędna wartość. Wybierz pozycję w przedziale 1-9: ")
 
+    position = int(position) - 1
+
+    if board[position] != "_":
+        print("To miejsce na planszy jest już zajęte")
+
+    board[position] != player
+    display_board()
