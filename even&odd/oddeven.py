03e9517a40e14db0d11e3421a80932e8020da13b
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-11-30 12:40:55 +0100

    Update oddeven.py

--- even&odd/oddeven.py
+++ even&odd/oddeven.py
@@ -1,10 +1,7 @@
-i = 1
 end = int(input("podaj wartość końcową: "))
-while i <= end:
+for i in range(1, end + 1):
     print(i, end=" ")
     if i % 2 == 0:
-        print("- even")
+        print("- parzysta")
     else:
-        print("- odd")
-                
-    i += 1
+        print("- nieparzysta")
